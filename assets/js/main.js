ea15a50047acfa73fd1e37e0c9ded5e32313e48a
Danila Egorov <c4nsero@mail.ru>
2021-10-03 18:31:16 +0300

    update css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,37 @@
-(function () {
+(function() {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim();
-    if (all) {
-      return [...document.querySelectorAll(el)];
-    } else {
-      return document.querySelector(el);
-    }
+      el = el.trim();
+      if (all) {
+          return [...document.querySelectorAll(el)];
+      } else {
+          return document.querySelector(el);
+      }
   };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach((e) => e.addEventListener(type, listener));
-      } else {
-        selectEl.addEventListener(type, listener);
+      let selectEl = select(el, all);
+      if (selectEl) {
+          if (all) {
+              selectEl.forEach((e) => e.addEventListener(type, listener));
+          } else {
+              selectEl.addEventListener(type, listener);
+          }
       }
-    }
   };
 
   /**
    * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener("scroll", listener);
+      el.addEventListener("scroll", listener);
   };
 
   /**
@@ -39,20 +39,20 @@
    */
   let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
-    let position = window.scrollY + 200;
-    navbarlinks.forEach((navbarlink) => {
-      if (!navbarlink.hash) return;
-      let section = select(navbarlink.hash);
-      if (!section) return;
-      if (
-        position >= section.offsetTop &&
-        position <= section.offsetTop + section.offsetHeight
-      ) {
-        navbarlink.classList.add("active");
-      } else {
-        navbarlink.classList.remove("active");
-      }
-    });
+      let position = window.scrollY + 200;
+      navbarlinks.forEach((navbarlink) => {
+          if (!navbarlink.hash) return;
+          let section = select(navbarlink.hash);
+          if (!section) return;
+          if (
+              position >= section.offsetTop &&
+              position <= section.offsetTop + section.offsetHeight
+          ) {
+              navbarlink.classList.add("active");
+          } else {
+              navbarlink.classList.remove("active");
+          }
+      });
   };
   window.addEventListener("load", navbarlinksActive);
   onscroll(document, navbarlinksActive);
@@ -61,11 +61,11 @@
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
-    let elementPos = select(el).offsetTop;
-    window.scrollTo({
-      top: elementPos,
-      behavior: "smooth",
-    });
+      let elementPos = select(el).offsetTop;
+      window.scrollTo({
+          top: elementPos,
+          behavior: "smooth",
+      });
   };
 
   /**
@@ -73,58 +73,58 @@
    */
   let backtotop = select(".back-to-top");
   if (backtotop) {
-    const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add("active");
-      } else {
-        backtotop.classList.remove("active");
-      }
-    };
-    window.addEventListener("load", toggleBacktotop);
-    onscroll(document, toggleBacktotop);
+      const toggleBacktotop = () => {
+          if (window.scrollY > 100) {
+              backtotop.classList.add("active");
+          } else {
+              backtotop.classList.remove("active");
+          }
+      };
+      window.addEventListener("load", toggleBacktotop);
+      onscroll(document, toggleBacktotop);
   }
 
   /**
    * Mobile nav toggle
    */
-  on("click", ".mobile-nav-toggle", function (e) {
-    select("body").classList.toggle("mobile-nav-active");
-    this.classList.toggle("bi-list");
-    this.classList.toggle("bi-x");
+  on("click", ".mobile-nav-toggle", function(e) {
+      select("body").classList.toggle("mobile-nav-active");
+      this.classList.toggle("bi-list");
+      this.classList.toggle("bi-x");
   });
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
   on(
-    "click",
-    ".scrollto",
-    function (e) {
-      if (select(this.hash)) {
-        e.preventDefault();
+      "click",
+      ".scrollto",
+      function(e) {
+          if (select(this.hash)) {
+              e.preventDefault();
 
-        let body = select("body");
-        if (body.classList.contains("mobile-nav-active")) {
-          body.classList.remove("mobile-nav-active");
-          let navbarToggle = select(".mobile-nav-toggle");
-          navbarToggle.classList.toggle("bi-list");
-          navbarToggle.classList.toggle("bi-x");
-        }
-        scrollto(this.hash);
-      }
-    },
-    true
+              let body = select("body");
+              if (body.classList.contains("mobile-nav-active")) {
+                  body.classList.remove("mobile-nav-active");
+                  let navbarToggle = select(".mobile-nav-toggle");
+                  navbarToggle.classList.toggle("bi-list");
+                  navbarToggle.classList.toggle("bi-x");
+              }
+              scrollto(this.hash);
+          }
+      },
+      true
   );
 
   /**
    * Scroll with ofset on page load with hash links in the url
    */
   window.addEventListener("load", () => {
-    if (window.location.hash) {
-      if (select(window.location.hash)) {
-        scrollto(window.location.hash);
+      if (window.location.hash) {
+          if (select(window.location.hash)) {
+              scrollto(window.location.hash);
+          }
       }
-    }
   });
 
   /**
@@ -132,15 +132,15 @@
    */
   const typed = select(".typed");
   if (typed) {
-    let typed_strings = typed.getAttribute("data-typed-items");
-    typed_strings = typed_strings.split(",");
-    new Typed(".typed", {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000,
-    });
+      let typed_strings = typed.getAttribute("data-typed-items");
+      typed_strings = typed_strings.split(",");
+      new Typed(".typed", {
+          strings: typed_strings,
+          loop: true,
+          typeSpeed: 90,
+          backSpeed: 51,
+          backDelay: 1999,
+      });
   }
 
   /**
@@ -148,27 +148,27 @@
    */
   let skilsContent = select(".skills-content");
   if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
+      new Waypoint({
+          element: skilsContent,
+          offset: "80%",
+          handler: function(direction) {
+              let progress = select(".progress .progress-bar", true);
+              progress.forEach((el) => {
+                  el.style.width = el.getAttribute("aria-valuenow") + "%";
+              });
+          },
+      });
   }
 
   /**
    * Animation on scroll
    */
   window.addEventListener("load", () => {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true,
-      mirror: false,
-    });
+      AOS.init({
+          duration: 1000,
+          easing: "ease-in-out",
+          once: true,
+          mirror: false,
+      });
   });
 })();
