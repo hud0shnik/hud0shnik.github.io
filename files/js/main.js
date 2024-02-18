53432c7208a062d56407332938c34309b58d287c
Danila Egorov <c4nsero@mail.ru>
2024-02-18 20:12:06 +0300

    update title: minor fixes
    Двадцать один годик уже

--- files/js/main.js
+++ files/js/main.js
@@ -152,11 +152,11 @@
 	document.getElementById('switch-button').onclick = function () { 
 		if (!isNew){
 			document.getElementById('theme_css').href = '../files/css/styleNew.css';
-			document.title = 'Красивый сайт Дани Егорова | 20 лет принципиальности';
+			document.title = 'Красивый сайт Дани Егорова | 21 год принципиальности';
 			isNew = true;
 		}else{
 			document.getElementById('theme_css').href = '../files/css/style.css';
-			document.title = 'Сайт Дани Егорова | 20 лет принципиальности';
+			document.title = 'Сайт Дани Егорова | 21 год принципиальности';
 			isNew = false;
 		}
 	};
